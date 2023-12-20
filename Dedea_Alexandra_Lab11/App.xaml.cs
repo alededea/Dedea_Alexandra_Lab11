53809f2e8aba04169674686094867c11bdbddfd7
Alexandra Dedea <dedea.alexandra19@outlook.com>
2023-12-20 21:51:59 +0200

    Add project files.

---
+++ Dedea_Alexandra_Lab11/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Dedea_Alexandra_Lab11.Data;
+namespace Dedea_Alexandra_Lab11
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+
+        }
+    }
+}
---
+++ Dedea_Alexandra_Lab11/Data/IRestService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dedea_Alexandra_Lab11.Models;
+
+namespace Dedea_Alexandra_Lab11.Data
+{
+    public interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+        Task DeleteShopListAsync(int id);
+    }
+}
